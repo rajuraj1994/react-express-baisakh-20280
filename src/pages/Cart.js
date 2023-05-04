fc92ecb9996f3b9597df2372a98b7878ecc41459
rajuraj1994 <rajurajkandel@gmail.com>
2023-05-04 08:59:46 +0545

    thirteenth day class

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,58 +1,105 @@
-import React from 'react'
+import React, { useState, useEffect,Fragment } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { IMG_URL } from '../config';
 
 const Cart = () => {
-  return (
-    <>
-    <div className="container">
-        <h2 className="text-center text-muted mt-3">Your Cart Items</h2>
-        <div className="row justify-content-between mt-5 mb-3">
-            <div className="col-md-8 shadow-lg">
-                <hr/>
-                <div className="d-flex  p-3 align-items-center">
-                    <div className="col-3">
-                        <img src="https://image.shutterstock.com/image-photo/poznan-pol-jul-20-2020-260nw-1782856739.jpg"
-                            alt="" className="img-fluid" width="150"/>
-                    </div>
-                    <div className="col-3">
-                        <span><b>DELL</b></span>
-                    </div>
-                    <div className="col-2">
-                        <span className="text-warning">Rs.90000</span>
-                    </div>
-                    <div className="col-3">
-                        <div className="d-flex">
-                            <button className="btn btn-danger">-</button> &nbsp;
-                            <input type="number" name="qty" value="2" readonly className="form-control border-0"/>
-                            &nbsp;
-                            <button className="btn btn-primary">+</button> &nbsp;
-                        </div>
+    const [products, setProducts] = useState([])
 
-                    </div>
-                    <div className="col-1">
-                        <button className="btn btn-danger">
-                            <i className="fa fa-trash"></i>
-                        </button>
-                    </div>
+    useEffect(() => {
+        const cartData = localStorage.getItem('cartItems')
+        setProducts(JSON.parse(cartData))
+    }, [])
+
+    //decrease quantity
+    const decreaseQty=id=>{
+        const updateProducts=products.map(item=>{
+            if(item.id===id && item.quantity>1){
+                return {...item,quantity:item.quantity-1}
+            }
+            return item
+        })
+        setProducts(updateProducts)
+        localStorage.setItem('cartItems',JSON.stringify(updateProducts))
+    } 
+
+    //increase quantity
+    const increaseQty=id=>{
+        const updateProducts=products.map(item=>{
+            if(item.id===id && item.quantity<item.countInStock){
+                return {...item,quantity:item.quantity+1}
+            }
+            return item
+        })
+        setProducts(updateProducts)
+        localStorage.setItem('cartItems',JSON.stringify(updateProducts))
+    } 
+
+    return (
+        <>
+            <ToastContainer theme='colored' position='top-center' />
+            <div className="container">
+
+                <div className="row justify-content-between my-5">
+                    {products && products.length == 0 ?
+                        <h2 className="text-center text-danger mt-3">Your Cart is Empty</h2>
+                        : (
+                            <>
+                                <h2 className="text-center text-muted mt-3">Your Cart Items</h2>
+                                <div className="col-md-8 shadow-lg">
+                                    {products && products.map((item,i)=>(
+                                        <Fragment key={i}>
+                                    <hr />
+                                    <div className="d-flex  p-3 align-items-center">
+                                        <div className="col-3">
+                                            <img src={`${IMG_URL}/${item.image}`}
+                                                alt={item.name} className="img-fluid" width="150" />
+                                        </div>
+                                        <div className="col-3">
+                                            <span><b>{item.name}</b></span>
+                                        </div>
+                                        <div className="col-2">
+                                            <span className="text-warning">Rs.{item.price}</span>
+                                        </div>
+                                        <div className="col-3">
+                                            <div className="d-flex">
+                                                <button className="btn btn-danger" onClick={()=>decreaseQty(item.id)}>-</button> &nbsp;
+                                                <input type="number" name="qty" value={item.quantity} readonly className="form-control border-0" />
+                                                &nbsp;
+                                                <button className="btn btn-primary" onClick={()=>increaseQty(item.id)}>+</button> &nbsp;
+                                            </div>
+
+                                        </div>
+                                        <div className="col-1">
+                                            <button className="btn btn-danger">
+                                                <i className="fa fa-trash"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                           
+                                    </Fragment>
+                                    ))}
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="shadow-lg p-2">
+                                        <h5>Cart Summary</h5>
+                                        <hr />
+                                        <span><b>Units:</b> {products.reduce((ac,item)=>(ac+Number(item.quantity)),0)}</span>
+                                        <br />
+                                        <span><b>Total:</b> Rs.{products.reduce((ac,item)=>(ac+item.quantity*item.price),0)}</span>
+                                        <hr />
+                                        <button className="btn btn-warning">CheckOut</button>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }
                 </div>
-                <hr />
-            </div>
-            <div className="col-md-3">
-                <div className="shadow-lg p-2">
-                    <h5>Cart Summary</h5>
-                    <hr />
-                    <span><b>Units:</b> 2</span>
-                    <br />
-                    <span><b>Total:</b> Rs.180000</span>
-                    <hr />
-                    <button className="btn btn-warning">CheckOut</button>
-                </div>
             </div>
 
-        </div>
-    </div>
-
-    </>
-  )
+        </>
+    )
 }
 
 export default Cart
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import { API, IMG_URL } from '../config'
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProductDetails = () => {
     const[product,setProduct]=useState({})
@@ -14,9 +16,34 @@ const ProductDetails = () => {
             setProduct(res.data)
         })
         .catch(err=>console.log(err))
-    })
+    },[])
+
+    // add to cart 
+    const addToCart=()=>{
+        const cartItems=JSON.parse(localStorage.getItem('cartItems')) || []
+        const productItem={
+            id:product._id,
+            name:product.product_name,
+            price:product.product_price,
+            image:product.product_image,
+            category:product.category,
+            description:product.product_description,
+            countInStock:product.countInStock,
+            quantity:1
+        }
+        const existingItem=cartItems.find(item=>item.id===product._id)
+        if(existingItem){
+            toast.error('Product already in the cart')
+        }
+        else{
+            cartItems.push(productItem)
+            localStorage.setItem('cartItems',JSON.stringify(cartItems))
+            toast.success(`${productItem.name} is added to cart`)
+        }
+    }
   return (
     <>
+    <ToastContainer theme='colored' position='top-center'/>
     <div className='container shadow p-5 my-5'>
         <div className='row d-flex justify-content-between'>
             <div className='col-md-5'>
@@ -28,7 +55,7 @@ const ProductDetails = () => {
                 <p>{product.product_description}</p>
                 <br/>
                 <br/>
-                <button className='btn btn-success'>Add to Cart</button>
+                <button className='btn btn-success' onClick={addToCart}>Add to Cart</button>
             </div>
         </div>
     </div>
